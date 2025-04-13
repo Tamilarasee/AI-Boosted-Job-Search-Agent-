ba41015700d95bf912a320951fffe40bd4e90f82
Tamilarasee Sethuraj <tamilaraseeeceskct@gmail.com>
2025-04-12 19:16:48 -0700

    updated logging for depoy error

--- utils/supabase/db.py
+++ utils/supabase/db.py
@@ -5,5 +5,5 @@ from dotenv import load_dotenv
 load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
-key: str = os.environ.get("SUPABASE_KEY")
+key: str = os.environ.get("SUPABASE_SERVICE_ROLE_KEY")
 supabase: Client = create_client(url, key)
